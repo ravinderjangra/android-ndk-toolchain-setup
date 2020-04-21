332fbdbc358c396d1d9a9ee34a198861619648ae
Ravinder Jangra <ravinderjangra@live.com>
2020-04-21 23:22:18 +0530

    use default ndk on macOS

--- src/ndk.ts
+++ src/ndk.ts
@@ -1,13 +1,23 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import * as os from 'os'
 
 export class Ndk {
-  private static readonly defaultNdkPath =
-    '/usr/local/lib/android/sdk/ndk-bundle/build/tools/make_standalone_toolchain.py'
+  osPlatform = os.platform()
   private readonly path: string
+  static defaultNdkPath: string
 
   private constructor(path: string) {
     this.path = path
+    const isOSX: boolean = this.osPlatform === 'darwin'
+    const isLinux: boolean = this.osPlatform === 'linux'
+    if (isLinux) {
+      Ndk.defaultNdkPath =
+        '/usr/local/lib/android/sdk/ndk-bundle/build/tools/make_standalone_toolchain.py'
+    } else if (isOSX) {
+      Ndk.defaultNdkPath =
+        '/Users/runner/Library/Android/sdk/ndk-bundle/build/tools/make_standalone_toolchain.py'
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
